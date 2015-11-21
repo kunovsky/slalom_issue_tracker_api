3ad27b6798a8d1985d339c8da9b8a410b09be049
Ty Kunovsky <tyson.kunovsky@slalom.com>
2015-11-20 22:49:52 -0600

    wip

--- app/models/defect_list.rb
+++ app/models/defect_list.rb
@@ -10,30 +10,9 @@ class DefectList < ActiveRecord::Base
       project_info[:defects] = []
       project_info[:defects_count] = 0
       project_info[:graph_data] = {}
-      project_info[:graph_data][:day] = {}
+      project_info[:graph_data][:days] = {}
       project_info[:priority_data] = {}
-
-      # # TODO: Remove this
-      # if project.name == "test_project"
-      #    project_info[:graph_data][:day][Date.parse('2015-11-17')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-11-16')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-11-15')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-11-14')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-10-14')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-9-15')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-9-17')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-8-17')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-11-13')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-11-12')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-10-17')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-10-16')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-10-15')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-10-15')] = 2
-      #    project_info[:graph_data][:day][Date.parse('2015-7-15')] = 2
-      #    project_info[:priority_data]["Highest"] = 8
-      #    project_info[:priority_data]["Medium"] = 10
-      #    project_info[:priority_data]["Lowest"] = 10
-      # end
+      project_info[:priority_graph_data] = {}
 
       project.issues.each do |issue|
         if issue.fields['issuetype']['name'] == BUG
@@ -42,11 +21,20 @@ class DefectList < ActiveRecord::Base
           date = Date.parse(issue.fields['created'])
           priority = issue.fields['priority']['name']
 
-          project_info[:graph_data][:day][date] = 0 unless project_info[:graph_data][:day][date]
-          project_info[:graph_data][:day][date] += 1
+          project_info[:graph_data][:days][date] = 0 unless project_info[:graph_data][:days][date]
+          project_info[:graph_data][:days][date] += 1
 
           project_info[:priority_data][priority] = 0 unless project_info[:priority_data][priority]
           project_info[:priority_data][priority] += 1
+
+          if !project_info[:priority_graph_data][priority]
+            project_info[:priority_graph_data][priority] = {}
+            project_info[:priority_graph_data][priority][:days] = {}
+          end
+
+          project_info[:priority_graph_data][priority][:days][date] = 0 unless
+            project_info[:priority_graph_data][priority][:days][date]
+          project_info[:priority_graph_data][priority][:days][date] += 1
 
           project_info[:defects].push({
             summary: issue.summary,
@@ -58,45 +46,114 @@ class DefectList < ActiveRecord::Base
           })
         end
       end
-      project_info[:graph_data][:day] = project_info[:graph_data][:day].sort.to_h
+
+      project_info[:graph_data][:days] =
+        self.populate_days_with_no_data(project_info[:graph_data][:days].sort.to_h)
+
+      project_info[:priority_data] =
+        self.transform_hash_to_array_of_hashes(project_info[:priority_data], :defect_count)
+
       data.push(project_info)
     end
     return self.aggregate_week_and_month_data(data)
   end
 
+  def self.populate_days_with_no_data(existing_days)
+    return {} unless existing_days.first
+    existing_days = self.force_week_to_start_on_monday(existing_days)
+    current = existing_days.keys.first
+    finish = existing_days.keys.last
+    until current == finish
+      existing_days[current] = 0 unless existing_days[current]
+      current = current + 1
+    end
+    return existing_days.sort.to_h
+  end
+
   def self.aggregate_week_and_month_data(data)
     data.each do |project_info|
 
-      project_info[:graph_data][:week] = {}
-      project_info[:graph_data][:month] = {}
+      storage = []
+      project_info[:graph_data][:weeks] = {}
+      project_info[:graph_data][:months] = {}
 
-      # TODO: Have week start on a monday
-      current_week =  nil
-      project_info[:graph_data][:day].each do |week, count|
-        if !current_week
-          current_week = week
-          project_info[:graph_data][:week][current_week] = count
-        elsif week <= current_week + 7
-          project_info[:graph_data][:week][current_week] += count
-        else
-          current_week = week
-          project_info[:graph_data][:week][current_week] = count
+      project_info[:graph_data][:weeks] =
+        self.aggregate_week_data(project_info[:graph_data][:days])
+      project_info[:graph_data][:months] =
+        self.aggregate_month_data(project_info[:graph_data][:weeks])
+
+      project_info[:priority_graph_data].each do |key, values|
+
+        project_info[:priority_graph_data][key][:days] =
+          self.populate_days_with_no_data(project_info[:priority_graph_data][key][:days].sort.to_h)
+
+        values[:days].each do |day, count|
+
+          project_info[:priority_graph_data][key][:weeks] =
+            self.aggregate_week_data(project_info[:priority_graph_data][key][:days])
+          project_info[:priority_graph_data][key][:months] =
+            self.aggregate_month_data(project_info[:priority_graph_data][key][:weeks])
         end
+        storage = []
+        storage.push(self.transform_hash_to_array_of_hashes(project_info[:priority_graph_data], :graph_data))
       end
+      project_info[:priority_graph_data] = storage.flatten
+    end
+    return {projects: data}
+  end
 
-      current_month = nil
-      project_info[:graph_data][:week].each do |date, count|
-        if !current_month
-          current_month = date.month
-          project_info[:graph_data][:month][current_month] = count
-        elsif current_month == date.month
-          project_info[:graph_data][:month][current_month] += count
-        else
-          current_month = date.month
-          project_info[:graph_data][:month][current_month] = count
-        end
+  def self.transform_hash_to_array_of_hashes(to_transform, key)
+    storage = []
+    to_transform.to_a.each do |data|
+      storage.push({
+        name: data[0],
+        "#{key}": data[1]
+        })
+    end
+    return storage
+  end
+
+  def self.force_week_to_start_on_monday(existing_days)
+    current = existing_days.keys.first
+    until current.monday?
+      current = current -1
+      existing_days[current] = 0
+    end
+    return existing_days.sort.to_h
+  end
+
+  def self.aggregate_week_data(days)
+    current_week =  nil
+    weeks = {}
+    days.each do |week, count|
+      if !current_week
+        current_week = week
+        weeks[current_week] = count
+      elsif week <= current_week + 6
+        weeks[current_week] += count
+      else
+        current_week = week
+        weeks[current_week] = count
       end
     end
-    return { projects: data }
+    return weeks
+  end
+
+  def self.aggregate_month_data(weeks)
+    return unless weeks
+    current_month = nil
+    months = {}
+    weeks.each do |date, count|
+      if !current_month
+        current_month = date.month
+        months[current_month] = count
+      elsif current_month == date.month
+        months[current_month] += count
+      else
+        current_month = date.month
+        months[current_month] = count
+      end
+    end
+    return months
   end
 end
